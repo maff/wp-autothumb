253925fbc1dd7308520731a7133cd295b2c26dde
Mathias Geat <mathias@ailoo.net>
2012-02-06 16:15:24 +0100

    some codestyle updates

--- autothumb.php
+++ autothumb.php
@@ -10,8 +10,10 @@ Author URI: http://ailoo.net/
 
 define('AUTOTHUMB_PATH', dirname(__FILE__));
 
-/* Initialization
-------------------------------------------------------------------------------------- */
+/**
+ * Initialization
+ * -----------------------------------------------------------------------------
+ */
 
 if (is_admin()) {
     add_action('admin_menu', 'autothumb_add_options_page');
@@ -19,14 +21,16 @@ if (is_admin()) {
     add_action('admin_init', 'autothumb_register_settings');
     add_action('admin_init', 'autothumb_generate_rewrite_rules');
 } else {
-    if(get_option('autothumb_apply_the_content') == 1) {
+    if (get_option('autothumb_apply_the_content') == 1) {
         add_filter('the_content', 'autothumb');
     }
 }
 
 
-/* Plugin
-------------------------------------------------------------------------------------- */
+/**
+ * Plugin
+ * -----------------------------------------------------------------------------
+ */
 
 /**
  * getphpthumburl
@@ -42,58 +46,58 @@ function getphpthumburl($image, $params = 'w=800', $xhtmlOutput = true)
 {
     include(AUTOTHUMB_PATH . '/phpthumb/phpThumb.config.php');
 
-    if(!empty($image)) {
+    if (!empty($image)) {
         $httpSrc = false;
-        if(strtolower(substr($image, 0, 1)) == '/') {
+        if (strtolower(substr($image, 0, 1)) == '/') {
             $image = str_replace($_SERVER['DOCUMENT_ROOT'], '/', $image);
-        } elseif(strtolower(substr($image, 0, 4)) == 'http') {
+        } elseif (strtolower(substr($image, 0, 4)) == 'http') {
             $wordpress_url = parse_url(get_bloginfo('url'));
             $image_url = parse_url($image);
-            
+
             // trim URL when it points to the same host
-            if($wordpress_url['host'] == $image_url['host']) {
+            if ($wordpress_url['host'] == $image_url['host']) {
                 $image = $image_url['path'];
                 $httpSrc = false;
             } else {
                 $httpSrc = true;
             }
         } else {
-            $blogurl = parse_url(get_bloginfo('wpurl'));                
+            $blogurl = parse_url(get_bloginfo('wpurl'));
             $image = $blogurl['path'] . '/' . $image;
-        }  
-    
+        }
+
         $queryString = 'src=' . $image . '&' . $params;
-        
+
         $clean_urls = false;
-        if(get_option('autothumb_clean_urls') == 1) {
+        if (get_option('autothumb_clean_urls') == 1) {
             $clean_urls = true;
         }
-        
-        if($clean_urls) {
+
+        if ($clean_urls) {
             $imageSeparator = '';
-            if($httpSrc) {
+            if ($httpSrc) {
                 $imageSeparator = '/';
             }
-        
+
             $query = get_bloginfo('wpurl') . '/' . get_option('autothumb_clean_urls_path') . $imageSeparator . $image . '?' . $params;
         } else {
             $query = get_bloginfo('wpurl') . '/wp-content/plugins/autothumb/image.php?' . $queryString;
         }
-        
+
         // generate hash for security
         $hash = md5($queryString . $PHPTHUMB_CONFIG['high_security_password']);
-        
+
         // append hash to query
-        $query .= '&hash=' . $hash;    
-        
-        if($xhtmlOutput) {
+        $query .= '&hash=' . $hash;
+
+        if ($xhtmlOutput) {
             // generate XHTML valid URLs
             $query = str_replace('&', '&amp;', $query);
         }
-        
+
         return $query;
-    }   
-    
+    }
+
     return false;
 }
 
@@ -109,122 +113,122 @@ function autothumb($content)
 {
     $imagesearch = array();
     $imagereplace = array();
-    
+
     $pattern = '/<img[^>]*>/';
     preg_match_all($pattern, $content, $toReplace);
-    
-    for($n = 0; $n < count($toReplace[0]); $n++)
-    {
+
+    for ($n = 0; $n < count($toReplace[0]); $n++) {
         $processImage = true;
         $imagetag = $toReplace[0][$n];
         $newtag = $imagetag;
-        
+
         $search = array();
         $result = array();
         $image = array();
-        
+
         $search['process'] = '/process="([0-1])"/';
         $search['src'] = '/src="([^"]*)"/';
         $search['width'] = '/width="[0-9]+"/';
         $search['height'] = '/height="[0-9]+"/';
         $search['imagephp'] = '/image.php/';
         $search['phpthumb'] = '/phpThumb.php/';
-        
+
         preg_match($search['process'], $imagetag, $result['process']);
-        
+
         // check process html attribute...abort if false
-        if(count($result['process']) == 2) {
-            if($result['process'][1] == 0) {
+        if (count($result['process']) == 2) {
+            if ($result['process'][1] == 0) {
                 $processImage = false;
             }
-            
+
             $newtag = preg_replace('/ process="[0-1]"/', '', $newtag);
             $newtag = str_replace('  ', ' ', $newtag);
         }
-            
-        if($processImage) {       
+
+        if ($processImage) {
             preg_match($search['src'], $imagetag, $result['src']);
             preg_match($search['width'], $imagetag, $result['width']);
             preg_match($search['height'], $imagetag, $result['height']);
-            
+
             $result['src'][1] = trim($result['src'][1]);
-            if(!empty($result['src'][1])) {
+            if (!empty($result['src'][1])) {
                 $image['src'] = $result['src'][1];
-                
-                if(!empty($result['width'][0])) {
+
+                if (!empty($result['width'][0])) {
                     $image['width'] = str_replace('width="', '', $result['width'][0]);
                     $image['width'] = trim(str_replace('"', '', $image['width']));
                 }
-                
-                if(!empty($result['height'][0])) {
+
+                if (!empty($result['height'][0])) {
                     $image['height'] = str_replace('height="', '', $result['height'][0]);
                     $image['height'] = trim(str_replace('"', '', $image['height']));
                 }
-                
+
                 preg_match($search['imagephp'], $image['src'], $result['imagephp']);
                 preg_match($search['phpthumb'], $image['src'], $result['phpthumb']);
 
-                if(count($result['imagephp']) == 0 && count($result['phpthumb']) == 0) {
+                if (count($result['imagephp']) == 0 && count($result['phpthumb']) == 0) {
                     $ptoptions = array();
-                    if(!empty($image['width'])) $ptoptions[] = 'w=' . $image['width'];
-                    if(!empty($image['height'])) $ptoptions[] = 'h=' . $image['height'];
-                    
+                    if (!empty($image['width']))
+                        $ptoptions[] = 'w=' . $image['width'];
+                    if (!empty($image['height']))
+                        $ptoptions[] = 'h=' . $image['height'];
+
                     // thanks to netProphET for this addition
                     // this allows you to set phpthumb parameters in the image URL
                     // see http://modxcms.com/forums/index.php/topic,14858.msg102750.html#msg102750
-                    if(preg_match("/^([^\?]*)\?(.*)$/", $image['src'], $ma)) {
+                    if (preg_match("/^([^\?]*)\?(.*)$/", $image['src'], $ma)) {
                         $aParam = array();
                         $image['src'] = $ma[1];
-                        
+
                         $ma[2] = str_replace('{}', '[]', $ma[2]);
                         parse_str(urldecode($ma[2]), $aParam);
-                        
-                        foreach($aParam as $k => $param) {
+
+                        foreach ($aParam as $k => $param) {
                             // clean parameter keys
                             $k = str_replace('#038;', '', $k);
                             $k = str_replace('amp;', '', $k);
-                            
-                            if(is_array($param) && count($param) > 0)
-                            {
-                                foreach($param as $element) {
+
+                            if (is_array($param) && count($param) > 0) {
+                                foreach ($param as $element) {
                                     $ptoptions[] = "{$k}[]={$element}";
                                 }
-                            }
-                            else
-                            {
-                                if($k == 'process' && $param == '0') {
+                            } else {
+                                if ($k == 'process' && $param == '0') {
                                     $processImage = false;
                                 }
-                                
+
                                 $ptoptions[] = "{$k}={$param}";
                             }
                         }
                     }
+
                     unset($aParam, $param);
-                    
+
                     // call phpthumb only if there are any parameters set
-                    if(count($ptoptions) == 0) {
+                    if (count($ptoptions) == 0) {
                         $processImage = false;
                     } else {
                         $additional_options = array();
-                    
+
                         // allow enlargement of images, uncomment this if you want
                         $additional_options[] = 'aoe=1';
-                        
+
                         // best quality
                         $additional_options[] = 'q=100';
-                        
+
                         // merge default options and overwrite default options if necessary
                         $phpthumb_options = array_merge($additional_options, $ptoptions);
                     }
-                    
-                    if($processImage) {
+
+                    if ($processImage) {
                         $ptoptionstring = '';
-                        for($i = 0; $i < count($phpthumb_options); $i++) {
-                            if($i != 0) $ptoptionstring .= '&';
+                        for ($i = 0; $i < count($phpthumb_options); $i++) {
+                            if ($i != 0)
+                                $ptoptionstring .= '&';
                             $ptoptionstring .= $phpthumb_options[$i];
                         }
-                        
+
                         $newsrc = getphpthumburl($image['src'], $ptoptionstring);
                         $newtag = preg_replace('/src="([^"]*)"/', 'src="' . $newsrc . '"', $newtag);
                         $newtag = preg_replace('/ width="[^"]*"/', '', $newtag);
@@ -234,24 +238,24 @@ function autothumb($content)
                 }
             }
         }
-        
-        if($newtag != $imagetag) {
+
+        if ($newtag != $imagetag) {
             $imagesearch[$n] = $imagetag;
             $imagereplace[$n] = $newtag;
-        }        
+        }
     }
-    
-    if(count($imagesearch) > 0 && count($imagesearch) == count($imagereplace)) {
+
+    if (count($imagesearch) > 0 && count($imagesearch) == count($imagereplace)) {
         $content = str_replace($imagesearch, $imagereplace, $content);
     }
-    
+
     return $content;
 }
 
-
-/* Option Panel (thanks to Ben for the idea - see comments on plugin home page)
-------------------------------------------------------------------------------------- */
-
+/**
+ * Option Panel (thanks to Ben for the idea - see comments on plugin home page)
+ * -----------------------------------------------------------------------------
+ */
 
 function autothumb_add_options_page()
 {
@@ -262,19 +266,19 @@ function autothumb_register_settings()
 {
     register_setting('autothumb', 'autothumb_high_security_password', 'autothumb_high_security_password_update');
     register_setting('autothumb', 'autothumb_apply_the_content');
-    register_setting('autothumb', 'autothumb_clean_urls');    
-    register_setting('autothumb', 'autothumb_clean_urls_path', 'autothumb_clean_urls_path_update');    
+    register_setting('autothumb', 'autothumb_clean_urls');
+    register_setting('autothumb', 'autothumb_clean_urls_path', 'autothumb_clean_urls_path_update');
 }
 
 function autothumb_check()
 {
-    if(!is_writable(AUTOTHUMB_PATH . '/phpthumb/phpThumb.config.php')) {
+    if (!is_writable(AUTOTHUMB_PATH . '/phpthumb/phpThumb.config.php')) {
         add_action('admin_notices', create_function('', "echo '<div class=\"error\"><p>AutoThumb is not able to write to the phpThumb.config.php file. Please fix file permissions or edit the file manually.</p></div>';"));
     }
 
     $high_security_password = get_option('autothumb_high_security_password');
-    if($high_security_password === false || empty($high_security_password)) {
-        add_action('admin_notices', create_function('', "echo '<div class=\"error\"><p>AutoThumb High Security Password is not set. Please update your <a href=\"" . get_bloginfo('wpurl') . "/wp-admin/options-general.php?page=autothumb.php\">settings</a>.</p></div>';"));    
+    if ($high_security_password === false || empty($high_security_password)) {
+        add_action('admin_notices', create_function('', "echo '<div class=\"error\"><p>AutoThumb High Security Password is not set. Please update your <a href=\"" . get_bloginfo('wpurl') . "/wp-admin/options-general.php?page=autothumb.php\">settings</a>.</p></div>';"));
     }
 }
 
@@ -287,34 +291,34 @@ function autothumb_options_subpanel()
 function autothumb_high_security_password_update($password)
 {
     $password = trim($password);
-    if(strlen($password) < 5) return;
+    if (strlen($password) < 5)
+        return;
 
     $configFile = AUTOTHUMB_PATH . '/phpthumb/phpThumb.config.php';
 
-    if(is_writable($configFile)) {
+    if (is_writable($configFile)) {
         $config = file($configFile);
-        
+
         $needle = "/^\s*\\\$PHPTHUMB\_CONFIG\[\'high_security_password.*/";
-        
+
         $result = null;
         $i = 0;
-        foreach($config as $line){
+        foreach ($config as $line) {
             $line = rtrim($line, "\r\n") . PHP_EOL;
-            if(preg_match($needle, $line)) {
+            if (preg_match($needle, $line)) {
                 $result = $i;
                 break;
             }
-                
             ++$i;
         }
-        
-        if($result !== null) {
+
+        if ($result !== null) {
             $config[$result] = "\$PHPTHUMB_CONFIG['high_security_password']   = '" . $password . "';  // required if 'high_security_enabled' is true, must be at least 5 characters long" . PHP_EOL;
         }
 
         file_put_contents($configFile, implode($config));
     }
-    
+
     return $password;
 }
 
@@ -328,15 +332,15 @@ function autothumb_clean_urls_path_update($path)
 function autothumb_generate_rewrite_rules($_switch = false, $_path = false, $_flush = false)
 {
     global $wp_rewrite;
-    
+
     $switch = ($_switch !== false) ? $_switch : get_option('autothumb_clean_urls');
     $path = ($_path !== false) ? $_path : get_option('autothumb_clean_urls_path');
-    
-    if($switch == 1 && $path !== false && !empty($path)) {
+
+    if ($switch == 1 && $path !== false && !empty($path)) {
         $wp_rewrite->non_wp_rules = array($path . '/(.*)$' => 'wp-content/plugins/autothumb/image.php?$1');
     }
-    
-    if($_flush) {
+
+    if ($_flush) {
         $wp_rewrite->flush_rules();
     }
 }
